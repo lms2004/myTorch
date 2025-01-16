6a4186c551d3b6375d087806a8133847ce7f77a6
llms26 <mxylms1210@163.com>
2025-01-16 21:22:57 +0800

    feat: add softmax loss func

---
+++ loss_function.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def softmax_loss(Z, y):
+    """ Return softmax loss.  Note that for the purposes of this assignment,
+    you don't need to worry about "nicely" scaling the numerical properties
+    of the log-sum-exp computation, but can just compute this directly.
+
+    Args:
+        Z (np.ndarray[np.float32]): 2D numpy array of shape
+            (batch_size, num_classes), containing the logit predictions for
+            each class.
+        y (np.ndarray[np.uint8]): 1D numpy array of shape (batch_size, )
+            containing the true label of each example.
+
+    Returns:
+        Average softmax loss over the sample.
+    """
+    ### BEGIN YOUR CODE
+    return np.mean(np.log(np.sum(np.exp(Z),axis=1)) - Z[np.arange(Z.shape[0]), y])
+    ### END YOUR CODE
